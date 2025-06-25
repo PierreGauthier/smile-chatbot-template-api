73d3f2b4565ba49022093137e3debe09bafdaf8a
Alejandro <alejandro.reyes@smile.fr>
2025-06-25 11:49:55 +0200

    refactoring

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -7,7 +7,7 @@ from .database.cosmosdb_history_service import CosmosDbHistoryService
 from .azure_ad_authorization_service import AzureADAuthorizationService
 from .database.database_document_service import DatabaseDocumentService
 from .chat_service import ChatService
-from .vector_store_service import VectorStoreService
+from .vector_search.vector_store_service import VectorStoreService
 from .llm.embeddings_service import EmbeddingsService
 from .llm.llm_service import LlmService
 
@@ -15,7 +15,7 @@ from .llm.azure_openai_llm_service import AzureOpenAiLlmService
 from .database.cosmosdb_document_service import CosmosDbDocumentService
 from .llm.azure_openai_embeddings_service import AzureOpenAIEmbeddingsService
 from .llm.openai_embeddings_service import OpenAIEmbeddingsService
-from .azure_search_vector_store_service import AzureSearchVectorStoreService
+from .vector_search.azure_search_vector_store_service import AzureSearchVectorStoreService
 
 from .default_rag_chat_service import DefaultRagChatService
 
