740ea543cf16381c5700936312ca552e1a155e78
Alejandro <alejandro.reyes@smile.fr>
2025-09-11 10:15:54 +0200

    Saving

--- src/services/conversational_search/attribute_detection_manager.py
+++ src/services/conversational_search/attribute_detection_manager.py
@@ -15,7 +15,7 @@ class AttributeDetectionManager:
             self,
             attribute_set_db_service : Annotated[DatabaseAttributesSetupService, Depends(inject_attribute_database_service)],
             attribute_set_extraction_agent: Annotated[AttributeSetExtractionAgent, Depends(AttributeSetExtractionAgent)],
-            logger: Annotated[ContextLogger, Depends(partial(inject_logger, module_name="ConversationManager"))]):
+            logger: Annotated[ContextLogger, Depends(partial(inject_logger, module_name="AttributeDetectionManager"))]):
         self.attribute_set_db_service = attribute_set_db_service
         self.attribute_set_extraction_agent = attribute_set_extraction_agent
         self.logger = logger
